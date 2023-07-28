58640d1d27d75d0421eda16e8c1f6caf7081370a
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-07-27 22:35:48 -0700

    added API

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -8,12 +8,24 @@ const [error, setError] = useState(null)
 
 async function handleSubmit (e){
     e.preventDefault();
-    
+    try{
+        const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup',{
+            method: 'POST',
+             headers: {
+          'Content-Type': 'application/json'},
+             body: JSON.stringify({ username: userName, password })
+        });
+        const result = await response.json()
+        console.log('API RESULT', result)
+    }catch(error){
+        setError(error.message)
+    }
 
 }
 return (
 <form onSubmit={handleSubmit}>
 <h2> Sign Up!</h2>
+{error && <p> {error}</p>}
 <label>
     Username: <input value={userName} onChange={(e)=> setUserName(e.target.value)}/>
 </label>
